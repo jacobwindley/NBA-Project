77f4b806fe910f24087011b0a8422d0fc9a08320
Jake Windley <jacob.windley@gmail.com>
2024-07-25 09:50:05 -0600

    Initial upload

---
+++ data.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+Finals_and_MVP_df = pd.read_csv("NBA Finals and MVP.csv")
+MVP_df = pd.read_csv("NBA_Dataset.csv")
+historical_rap_player_df = pd.read_csv("historical_RAPTOR_by_player.csv")
+modern_rap_player_df = pd.read_csv("modern_RAPTOR_by_player.csv")
+latest_rap_player_df = pd.read_csv("latest_RAPTOR_by_player.csv")
+
